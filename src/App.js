2a336c1fd67b1bc60c7c0702299ec72682588799
yagoh2303 <yago.henriquess@hotmail.com>
2022-11-21 11:41:18 -0300

    all of hero elements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from './components/Navbar';
 import Hero from './components/Hero';
 import "./App.scss";
@@ -7,21 +7,30 @@ import CardList from './components/CardList';
 import CenteredButton from './components/CenteredButton';
 import Institutional from './components/Institutional';
 import Faq from './components/FAQ'
+import Footer from './components/Footer'
 
 import posts from './data/posts';
+import AccountModal from './components/AccountModal';
 
-const  App = () => (
+const  App = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
     <div className="App">
       <Navigation />
       <Hero />
       <CreditCard />
       <CardList posts={posts} />
-      <CenteredButton>Abra sua conta</CenteredButton>
+      <CenteredButton onClick={() => setShowModal(true)}>
+        Abra sua conta
+      </CenteredButton>
       <Institutional />
       <Faq />
+      <Footer />
+      <AccountModal show={showModal} handleClose={() => setShowModal(false)}  />
     </div>
   );
-
+}
 
 export default App;
 
